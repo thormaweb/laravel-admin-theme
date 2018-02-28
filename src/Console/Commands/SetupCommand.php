0a03f2d166c63e672ce6f6b359c414cd97a8884f
Emiliano Tisato <emilianotisato@gmail.com>
2018-02-28 17:59:48 -0300

    Fix command

--- src/Console/Commands/SetupCommand.php
+++ src/Console/Commands/SetupCommand.php
@@ -21,25 +21,17 @@ class SetupCommand extends Command
     protected $description = 'Setup migration and models for Admin Theme';
 
     /**
-     * Commands to call with their description
-     *
-     * @var array
-     */
-    protected $calls = [
-        'admin-theme:add-user-trait' => 'Adding AdminThemeUserTrait to User model',
-        ['vendor:publish', ['--provider' => 'Spatie\Permission\PermissionServiceProvider', '--tag' => 'config']] => 'Publishing Permissions config file.'
-    ];
-
-    /**
      * Execute the console command.
      *
      * @return void
      */
     public function handle()
     {
-        foreach ($this->calls as $command => $info) {
-            $this->line(PHP_EOL . $info);
-            $this->call($command);
-        }
+        $this->line(PHP_EOL . 'Adding AdminThemeUserTrait to User model');
+        $this->call('admin-theme:add-user-trait');
+
+        $this->line(PHP_EOL . 'Publishing Permissions config file');
+        $this->call('vendor:publish', ['--provider' => 'Spatie\Permission\PermissionServiceProvider', '--tag' => 'config']);
+
     }
 }
